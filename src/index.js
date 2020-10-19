05e4e97780f7c766c4262a589c1132f4ab818367
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-10-19 12:13:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+module.exports = function check (str, bracketsConfig) {
+  let stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (const char of str.split('')) {
+      if (stack[stack.length - 1] === char) {
+          stack.pop();
+      } else if (brackets[char]) {
+          stack.push(brackets[char]);
+      } else {
+          return false;
+      }
+  }
+  return stack.length === 0;
 }
+
+
